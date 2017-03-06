dbebb3aef7d8e5d04c25b8a0060d1b34963d7c97
xylcbd <dahanhbm@163.com>
2017-03-06 23:47:12 +0800

    add namespace.

--- header/EasyCNN/EasyAssert.h
+++ header/EasyCNN/EasyAssert.h
@@ -8,5 +8,5 @@ namespace EasyCNN
 	void easyAssertCore(const std::string& file,const std::string& function,const long line,
 		const bool condition, const char* fmt, ...);
 #define easyAssert(condition,fmt,...) \
-	easyAssertCore(__FILE__,__FUNCTION__,__LINE__,(condition),(fmt),##__VA_ARGS__);
+	EasyCNN::easyAssertCore(__FILE__,__FUNCTION__,__LINE__,(condition),(fmt),##__VA_ARGS__);
 }
